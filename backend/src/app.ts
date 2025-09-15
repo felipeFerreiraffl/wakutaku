dc4111f7b244333c6aa335b97e52ace25b4f7216
felipeFerreiraffl <felipado2x0@gmail.com>
2025-09-15 20:10:46 -0300

    feat: created new exemple middleware for external api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,18 @@
 import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 app.use(express.json());
+
+// Criação de uma proxy
+const apiProxy = createProxyMiddleware({
+  target: "https://api.jikan.moe/v4",
+  changeOrigin: true,
+  pathRewrite: {
+    "^/api": "",
+  },
+});
+
+app.use("/api", apiProxy);
 
 export default app;
