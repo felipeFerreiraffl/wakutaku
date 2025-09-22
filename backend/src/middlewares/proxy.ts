a8aca085edea9d1b07755dbde0a86b4552073e08
felipeFerreiraffl <felipado2x0@gmail.com>
2025-09-21 21:51:49 -0300

    chore: new props for proxy configuration

--- backend/src/middlewares/proxy.ts
+++ backend/src/middlewares/proxy.ts
@@ -1,13 +1,25 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
+import type { Request, Response, NextFunction } from "express";
 
 // Configuração do middleware do proxy da Jikan
 const jikanProxy = createProxyMiddleware({
   target: "https://api.jikan.moe/v4",
-  changeOrigin: true,
+  changeOrigin: true, // Muda o header Host para o target
   pathRewrite: {
     "^/api": "",
   },
-  timeout: 10000,
+  timeout: 15000,
+  proxyTimeout: 15000,
+  on: {
+    proxyRes: (proxyRes, req) => {
+      const isStatic = req.url?.match(/(genres|producers|magazines)/); // Definição de URLs estáticas
+      const maxAge = isStatic ? 86400 : 300; // Definição do max-age do Cache-Control (tempo que a requisição se mantém no Header)
+      proxyRes.headers["Cache-Control"] = `public, max-age=${maxAge}`;
+    },
+    error: (err, req, res) => {
+      res.end("Erro os utilizar a proxy para Jikan API");
+    },
+  },
 });
 
 export default jikanProxy;
