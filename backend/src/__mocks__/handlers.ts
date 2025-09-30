fed9cc815a88730d2e9109d6bc17a03e6d0eac09
felipeFerreiraffl <felipado2x0@gmail.com>
2025-09-29 23:16:49 -0300

    feat: created new mocks folder for false api data

---
+++ backend/src/__mocks__/handlers.ts
@@ -0,0 +1,16 @@
+import { http, HttpResponse } from "msw";
+import { envVar } from "../config/envConfig.js";
+
+export const handlers = [
+  http.get(`http://localhost:${envVar.PORT}/api/season_stats`, () => {
+    return HttpResponse.json({
+      success: true,
+      data: {
+        totalCount: 200,
+        frequentGenre: "Action",
+        frequentDemography: "Shounen",
+        averageScore: 10,
+      },
+    });
+  }),
+];
